ea3d0209c6f5db38c162ce72419e0f210a594743
dongdong <286084845@qq.com>
2018-10-23 14:13:20 +0800

    前端js

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
-myButton.addEventListener('click', (e) => {
+window.jQuery = function (nodeOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () {}
+    nodes.html = function () {}
+    return nodes
+}
+
+
+window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) {
+
+    // let url = options.url
+    // let method = options.method
+    // let body = options.body
+    // let successFn = options.successFn
+    // let failFn = options.failFn
+    // let headers = options.headers
+
+    // es6 解构赋值
+    // 下面这一行等价于上面六行
+    
+
     let request = new XMLHttpRequest()
+    request.open(method, url) // 配置request
+    for (let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
-            console.log('请求响应都完毕了')
             if (request.status >= 200 && request.status < 300) {
-                console.log('请求成功')
-                console.log(request.responseText)
-                let string = request.responseText
-                // 把符合 JSON 语法的字符串
-                // 转换成 JS 对应的值
-                let object = window.JSON.parse(string)
-                console.log(typeof object)
-                console.log(object)
-                console.log(object.note.from)
-
+                successFn.call(undefined, request.responseText)
             } else if (request.status >= 400) {
-                console.log('请求失败')
+                failFn.call(undefined, request)
             }
         }
     }
-    request.open('GET', '/xxx') // 配置request
-    
-    request.send()
+    request.send(body)
+}
+
+window.$ = window.jQuery
+
+function f1(responseText) {}
+function f2(responseText) {}
+
+myButton.addEventListener('click', (e) => {
+    window.jQuery.ajax({
+        url: '/xxx',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'frank': '18'
+        },
+        successFn: (x) => {
+            f1.call(undefined, x)
+            f2.call(undefined, x)
+        },
+        failFn: () => {
+            console.log(x)
+            console.log(x.status)
+            console.log(x.responseText)
+        }
+    })
 })
